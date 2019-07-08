b419da74ba778900fb30c409656843886c034b37
Mike Pennisi <mike@mikepennisi.com>
2019-07-08 17:18:03 -0400

    Add debugging statement

--- tools/ci/update_pr_preview.py
+++ tools/ci/update_pr_preview.py
@@ -11,6 +11,12 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
+import subprocess
+logger.info(
+    subprocess.check_output(['git', 'rev-parse', 'HEAD'])
+)
+
+
 class Status(object):
     SUCCESS = 0
     FAIL = 1
